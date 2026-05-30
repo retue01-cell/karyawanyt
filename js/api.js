7d0dd6781370cf8e4671e6978fe6a08463e1865a
retue01-cell <retube101@gmail.com>
2026-05-30 20:58:32 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@
  * - Jika API_BASE_URL diisi → semua request dikirim ke Google Apps Script
  */
 
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxlT5Tej5NEdX4DdY8ugzGT1zMAga3j2Lb-QZicl6ArHsSgDqvl000gjESHsgS44KVJ/exec'; // Kosongkan untuk mode localStorage, isi dengan URL Web App GAS
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwaCDkBuIqSjXeQceGijwZweqZJZfkuCboZJ9tR1HP4dyK4ER-hIXnf5OBpVpXyglOx9Q/exec'; // Kosongkan untuk mode localStorage, isi dengan URL Web App GAS
 
 const api = {
 
